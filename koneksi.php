175e611b7320ca25aeb9634bebca0c47f0896ff2
Adhyatmika Eka Saputra <148543848+Dehayeka@users.noreply.github.com>
2025-09-06 20:51:52 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,14 @@
 <?php
 $servername = "localhost";
-$database = "u336544492_ydl";
-$username = "root";
-$password = "Ydl2024@pass";
+$database   = "u336544492_ydl";
+$username   = "u336544492_root";
+$password   = "Ydl2024@pass";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-if (!$conn){
-    die("koneksi error : " . mysql_connect_error());
-}else{
-    echo "";
+if (!$conn) {
+    die("Koneksi error : " . mysqli_connect_error());
+} else {
+    // echo "Koneksi berhasil"; // bisa dicoba dulu untuk testing
 }
+?>
