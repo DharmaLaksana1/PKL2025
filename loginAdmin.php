68fc29d84167b4377b5d2f5d24a1a54ac14f675b
Adhyatmika Eka Saputra <148543848+Dehayeka@users.noreply.github.com>
2025-09-09 18:51:26 +0700

    1

--- loginAdmin.php
+++ loginAdmin.php
@@ -1,20 +1,23 @@
 <?php
-session_start();
+session_start(); // Mulai sesi
+
 require "koneksi.php";
 
-$email = mysqli_real_escape_string($conn, $_POST["email"]);
-$password = mysqli_real_escape_string($conn, $_POST["password"]);
+$email = $_POST["email"];
+$password = $_POST["password"];
 
-$query_sql = "SELECT * FROM admin WHERE email_admin = '$email' AND sandi_admin = '$password'";
+$query_sql = "SELECT * FROM Admin
+              WHERE email_admin = '$email' AND sandi_admin = '$password'";
+
 $result = mysqli_query($conn, $query_sql);
 
-if (mysqli_num_rows($result) > 0) {
+if (mysqli_num_rows($result) > 0){
+    // Jika login berhasil, simpan username dalam sesi
     $row = mysqli_fetch_assoc($result);
-    $_SESSION["username"] = $row["nama_admin"];
-    header("Location: beranda.php");
-    exit();
+    $_SESSION["username"] = $row["nama_admin"]; // Menggunakan nama_admin sebagai username
+    header("location: beranda.php");
 } else {
     echo "<center><h1>Email atau Password Anda Salah. Silahkan Coba Login Kembali.</h1>
-          <button><strong><a href='loginAdmin.html'>Login</a></strong></button></center>";
+            <button><strong><a href='loginAdmin.html'>Login</a></strong></button></center>";
 }
 ?>
